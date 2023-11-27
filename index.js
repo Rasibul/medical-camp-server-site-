e419fdaaef07deb70f3d7de1c413f6322ee4cde6
Rasibul <rasibul179@gmail.com>
2023-11-27 12:24:13 +0600

    create userCollection And post method for post user and make a get method for get user

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const userCollection = client.db("medicalDB").collection("users")
     const medicalCampCollection = client.db("medicalDB").collection("meicalCamp")
     const reviewCollection = client.db("medicalDB").collection("review")
 
@@ -37,11 +38,27 @@ async function run() {
         res.send(result)
     })
 
-    
+
     app.get('/api/v1/review',async(req,res)=>{
         const result = await reviewCollection.find().toArray()
         res.send(result)
     })
+
+    app.get('/api/v1/users', async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+  })
+
+  app.post('/api/v1/users', async (req, res) => {
+      const user = req.body
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+          return res.send({ message: 'user alredy axist', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+  })
 
 
 
