5ac821d7543a9c6eca588e17937a6f82944edcc2
Rasibul <rasibul179@gmail.com>
2023-11-27 16:31:06 +0600

    get data in specifick id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -38,6 +38,15 @@ async function run() {
         res.send(result)
     })
 
+
+ 
+
+  app.get('/api/v1/all-camp/:id',async(req,res)=>{
+    const id = req.params.id
+    const query = {_id: new ObjectId(id)}
+    const result = await medicalCampCollection.findOne(query)
+    res.send(result)
+  })
 
     app.get('/api/v1/review',async(req,res)=>{
         const result = await reviewCollection.find().toArray()
